e27b8ba6d09d21e0eed19aa84f01b8d9cb61f5e1
kvoronkin <kvoronkin@nbcomgroup.ru>
2026-02-03 21:40:21 +0300

    feat[step-auth]: Adds auth provider for app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { RouterProvider } from './providers/router';
 import { QueryProvider } from './providers/query';
+import { AuthProvider } from './providers/auth';
 import App from './App';
 
 export const Application: React.FC = () => {
   return (
     <QueryProvider>
       <RouterProvider>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </RouterProvider>
     </QueryProvider>
   );
